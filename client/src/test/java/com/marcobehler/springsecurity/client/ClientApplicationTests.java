f97ce381a8c61650e382b1f9c0345f69456a7744
Andreas Eisele <ae@andreaseisele.com>
2020-08-14 12:17:29 +0200

    initial scaffold

---
+++ client/src/test/java/com/marcobehler/springsecurity/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcobehler.springsecurity.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
