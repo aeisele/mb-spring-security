4c18ab0faae95cdba532eb1049b0bfb8dc73bbed
Andreas Eisele <ae@andreaseisele.com>
2020-08-15 12:58:44 +0200

    rudimentary error handling on remote controller

--- client/src/main/java/com/marcobehler/springsecurity/client/web/RemoteController.java
+++ client/src/main/java/com/marcobehler/springsecurity/client/web/RemoteController.java
@@ -3,12 +3,15 @@ package com.marcobehler.springsecurity.client.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
@@ -32,13 +35,18 @@ public class RemoteController {
     public Mono<Object> getTransactionsRemotely(@AuthenticationPrincipal Object principal) {
         logger.info("doing remote call as principal {}", principal);
 
-        Mono<Object> bodyMono = webClient.get()
+        return webClient.get()
                 .uri(transactionsUri)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
+                .onStatus(HttpStatus::isError, this::handleError)
                 .bodyToMono(Object.class);
+    }
 
-        return bodyMono;
+    private Mono<? extends Throwable> handleError(ClientResponse response) {
+        HttpStatus status = response.statusCode();
+        logger.error("got http status {} during remote call: {}", status, status.getReasonPhrase());
+        return Mono.error(new ResponseStatusException(status));
     }
 
 }
